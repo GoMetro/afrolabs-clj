e2a3be62743ccdfe4bccddd8c8234a4f7c548a33
Pieter Breed <pieter.breed@gmail.com>
2021-08-17 20:56:52 +0200

    enhanced kafka-utils/load-messages-from-confluent-topic to stop consuming at last offsets

--- src/afrolabs/components/kafka/utilities.clj
+++ src/afrolabs/components/kafka/utilities.clj
@@ -2,32 +2,55 @@
   (:require [afrolabs.components.kafka :as k]
             [integrant.core :as ig]
             [clojure.data.json :as json]
+            [clojure.core.async :as csp]
             [taoensso.timbre :as timbre
              :refer [log  trace  debug  info  warn  error  fatal  report
                      logf tracef debugf infof warnf errorf fatalf reportf
-                     spy get-env]]))
+                     spy get-env]]
+            [java-time :as time]))
 
 (defn load-messages-from-confluent-topic
   [& {:keys [bootstrap-server
              topics
              nr-msgs
              api-key api-secret
-             extra-strategies]
+             extra-strategies
+             msg-filter]
       :or {nr-msgs          10
+           msg-filter       identity
            extra-strategies []}}]
   (let [loaded-msgs (atom nil)
         loaded-enough-msgs (promise)
+
+        last-progress-update (atom (time/instant))
+
+        caught-up-ch (csp/chan)
+        _ (csp/go (csp/<! caught-up-ch)
+                  (info "Caught up to the end of the subscribed topics, closing...")
+                  (deliver loaded-enough-msgs true))
 
         consumer-client
         (reify
           k/IConsumerClient
           (consume-messages
               [_ msgs]
-            (let [new-state (swap! loaded-msgs (partial apply conj) msgs)
+            (let [msgs (filter msg-filter msgs)
+                  new-state (swap! loaded-msgs (partial apply conj) msgs)
                   how-many (count new-state)]
-              (when (< nr-msgs how-many)
+
+              ;; do we have enough yet? is anything ever enough?
+              (when (and nr-msgs
+                         (< nr-msgs how-many))
                 (info "Indicating that we've received enough messages...")
-                (deliver loaded-enough-msgs true)))
+                (deliver loaded-enough-msgs true))
+
+              ;; give progress indicators
+              (let [t-now (time/instant)]
+                (when (time/after? t-now
+                                   (time/plus @last-progress-update
+                                              (time/seconds 30)))
+                  (info (str "Loaded " how-many " messages ..."))
+                  (reset! last-progress-update t-now))))
             nil))
 
         ig-cfg
@@ -37,7 +60,8 @@
           :strategies                     (concat [(k/ConfluentCloud :api-key api-key :api-secret api-secret)
                                                    (k/SubscribeWithTopicsCollection topics)
                                                    (k/FreshConsumerGroup)
-                                                   (k/OffsetReset "earliest")]
+                                                   (k/OffsetReset "earliest")
+                                                   (k/CaughtUpNotifications caught-up-ch)]
                                                   extra-strategies)}}
 
         system (ig/init ig-cfg)]
@@ -56,146 +80,4 @@
         (ig/halt! system)
         (info "Extraordinary system stop completed.")))))
 
-(comment
 
-  (defonce username-password (atom nil))
-  (defn set-creds [un pw] (reset! username-password [un pw]))
-
-  (def ctrack-msgs (load-messages-from-topic :topics ["dataprovider_ctrack"]
-                                             :bootstrap-server "pkc-l6wr6.europe-west2.gcp.confluent.cloud:9092"
-                                             :api-key (first @username-password)
-                                             :api-secret (second @username-password)
-                                             :nr-msgs 1000))
-  (count ctrack-msgs)
-
-  (def netstar-msgs (load-messages-from-topic :topics ["dataprovider_netstar"]
-                                              :bootstrap-server "pkc-l6wr6.europe-west2.gcp.confluent.cloud:9092"
-                                              :api-key (first @username-password)
-                                              :api-secret (second @username-password)
-                                              :nr-msgs 1000))
-  (count netstar-msgs)
-
-  (first netstar-msgs)
-
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-  (defn sample [msgs & {:keys [how-many]}]
-    (into []
-          (comp
-           (if how-many (take how-many) identity)
-           (map #(update % :value json/read-str))
-           (map #(update-in % [:value "MessageData"] json/read-str))
-           (map :value))
-          msgs))
-
-  (require '[malli.provider :as mp])
-
-  (def ctrack-sample (sample ctrack-msgs :how-many 20))
-  (def netstar-sample (sample netstar-msgs :how-many 20))
-
-  (first ctrack-sample)
-  (second netstar-sample)
-
-  (mp/provide ctrack-sample)
-  (mp/provide netstar-sample)
-
-
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-  (require '[malli.json-schema :as mjson])
-
-  ;; for future reference
-  (def ctrack-schema [:map
-                      ["MessageData"
-                       [:map
-                        ["DriverId" {:optional true} string?]
-                        ["Ignition" boolean?]
-                        ["Longitude" double?]
-                        ["LocationString" string?]
-                        ["NodeId" int?]
-                        ["Speed" {:optional true} int?]
-                        ["StatusText" string?]
-                        ["DeliveryTag" int?]
-                        ["EventTimeUTC" string?]
-                        ["VehicleHeading" string?]
-                        ["Odo" int?]
-                        ["SingleStatus" int?]
-                        ["Latitude" double?]
-                        ["StreetMaxSpeed" {:optional true} int?]]]
-                      ["AttributesMap" [:map]]])
-
-  (mjson/transform ctrack-schema)
-
-  (def netstar-schema [:map
-                       ["MessageData"
-                        [:map
-                         ["DriverId" string?]
-                         ["$type" string?]
-                         ["Address" nil?]
-                         ["Battery" double?]
-                         ["Temperatures" [:map ["$type" string?] ["$values" [:vector any?]]]]
-                         ["IsIgnitionOn" boolean?]
-                         ["Imei" int?]
-                         ["Longitude" double?]
-                         ["HardwareAttachedGpsNotPresent" string?]
-                         ["AccidentId" int?]
-                         ["Altitude" double?]
-                         ["GpsLock" nil?]
-                         ["EngineHours" double?]
-                         ["Speed" double?]
-                         ["HardwareAttached" string?]
-                         ["TotalOdometer" double?]
-                         ["Odometer" double?]
-                         ["NoofSatellites" nil?]
-                         ["UnitType" int?]
-                         ["Samples" [:map ["$type" string?] ["$values" [:vector any?]]]]
-                         ["AmbientTemperature" int?]
-                         ["Forces"
-                          [:map
-                           ["$type" string?]
-                           ["$values"
-                            [:vector
-                             [:map
-                              ["$type" string?]
-                              ["Forward" int?]
-                              ["Backward" int?]
-                              ["Left" int?]
-                              ["Right" int?]
-                              ["Up" int?]
-                              ["Down" int?]]]]]]
-                         ["Hdop" nil?]
-                         ["InputOutputs"
-                          [:map
-                           ["$type" string?]
-                           ["DigitalOutput3" boolean?]
-                           ["DigitalOutput4" boolean?]
-                           ["DigitalInput1" boolean?]
-                           ["AnalogueInput2" boolean?]
-                           ["DigitalInput3" boolean?]
-                           ["DigitalOutput1" boolean?]
-                           ["AnalogueInput3" boolean?]
-                           ["AnalogueInput4" boolean?]
-                           ["DigitalInput4" boolean?]
-                           ["AnalogueInput1" boolean?]
-                           ["DigitalInput2" boolean?]
-                           ["DigitalOutput2" boolean?]]]
-                         ["CanBus" nil?]
-                         ["ServerDateTime" string?]
-                         ["Latitude" double?]
-                         ["IsLastKnowLocation" boolean?]
-                         ["VbuNo" int?]
-                         ["HardwareAttachedUptime" string?]
-                         ["EngineTemperature" double?]
-                         ["EventId" int?]
-                         ["FuelUsed" int?]
-                         ["Id" string?]
-                         ["Heading" double?]
-                         ["HardwareAttachedNoGsmInternationalRoaming" string?]
-                         ["DateTime" string?]]]
-                       ["AttributesMap" [:map]]])
-
-  (mjson/transform netstar-schema)
-
-
-
-  )
