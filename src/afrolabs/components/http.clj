9feab7fb1f669c9ad25e33195ad8bf2f311dcfee
Pieter Breed <pieter.breed@gmail.com>
2021-08-26 13:18:21 +0200

    seperate http handler from health-endpoint provider

--- src/afrolabs/components/http.clj
+++ src/afrolabs/components/http.clj
@@ -5,7 +5,8 @@
             [afrolabs.components.health :as -health]
             [clojure.string :as str]
             [taoensso.timbre :as log]
-            [afrolabs.components.health :as -health])
+            [afrolabs.components.health :as -health]
+            )
   (:import [afrolabs.components IHaltable]
            [afrolabs.components.health IServiceHealthTripSwitch]))
 
@@ -26,39 +27,39 @@
                      #(> % 1024)
                      #(< % java.lang.Short/MAX_VALUE)))
 (s/def ::ip valid-ip4?)
-(s/def ::endpoint (s/and string?
-                         #(pos-int? (count %))
-                         #(str/starts-with? % "/")))
-(s/def ::health-endpoint-cfg (s/keys :req-un [::health-component
-                                              ::endpoint]
-                                     :opt-un [::port
-                                              ::ip]))
 
-(defn create-http-health
-  [{:keys [health-component
-           port
+(defprotocol IHttpRequestHandler
+  "Allows components to provide ring-like HTTP handler to an HTTP component."
+  (handle-http-request [_ req] "httpkit-compatible http handler."))
+(s/def ::http-request-handler #(satisfies? IHttpRequestHandler %))
+(s/def ::worker-thread-name-prefix (s/and string?
+                                          #(pos-int? (count %))))
+
+(s/def ::handlers (s/coll-of ::http-request-handler))
+(s/def ::http-component-cfg (s/keys :req-un [::handlers]
+                                    :opt-un [::port
+                                             ::ip
+                                             ::worker-thread-name-prefix]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn create-http-component
+  [{:keys [port
            ip
-           endpoint]
+           handlers
+           worker-thread-name-prefix]
     :or   {port 8000
            ip   "0.0.0.0"}
     :as   cfg}]
 
-  (s/assert ::health-endpoint-cfg cfg)
+  (s/assert ::http-component-cfg cfg)
 
-  (let [handler (fn [{:keys [uri]}]
-                  (let [h? (-health/healthy? health-component)]
-                    (cond
-                      (not= uri endpoint)
-                      {:status 404
-                       :body "Unknown path"}
-
-                      h? {:status 200
-                          :body   "Service is healthy."}
-
-                      (not h?) {:status 500
-                                :body   "Service is NOT healthy :("})))
+  (let [handler (apply some-fn
+                       (map #(partial handle-http-request
+                                      %)
+                            handlers))
         s (httpkit/run-server handler
-                              {:worker-name-prefix   "health-http-worker"
+                              {:worker-name-prefix   worker-thread-name-prefix
                                :error-logger         (fn [txt ex]
                                                        (if-not ex
                                                          (log/error txt)
@@ -76,28 +77,43 @@
       IHaltable
       (halt [_] (httpkit/server-stop! s)))))
 
-(-comp/defcomponent {::-comp/ig-kw       ::health-endpoint
-                     ::-comp/config-spec ::health-endpoint-cfg}
-  [cfg] (create-http-health cfg))
+(-comp/defcomponent {::-comp/config-spec ::http-component-cfg
+                     ::-comp/ig-kw       ::service}
+  [cfg] (create-http-component cfg))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(comment
+(s/def ::endpoint (s/and string?
+                         #(pos-int? (count %))
+                         #(str/starts-with? % "/")))
+(s/def ::health-endpoint-cfg (s/and ::http-component-cfg
+                                    (s/keys :req-un [::health-component
+                                                     ::endpoint])))
 
-  (def h? (atom true))
-  (reset! h? false)
-  (reset! h? true)
-  (def s (create-http-health {:health-component (reify
-                                                  IServiceHealthTripSwitch
-                                                  (indicate-unhealthy! [_ _])
-                                                  (wait-while-healthy [_])
-                                                  (healthy? [_] @h?))
-                              :port 8002}))
+(defn create-http-health-endpoint
+  [{:keys [health-component
+           endpoint]
+    :as   cfg}]
 
-  (-comp/halt s)
+  (s/assert ::health-endpoint-cfg cfg)
 
+  (reify
+    IHttpRequestHandler
+    (handle-http-request
+        [_ {:keys [uri]}]
+      (when (= uri endpoint)
+        (if (-health/healthy? health-component)
+          {:status 200
+           :body   "Service is healthy."
+           :}
+          {:status 500
+           :body   "Service is NOT healthy :("})))))
 
+(-comp/defcomponent {::-comp/ig-kw       ::health-endpoint
+                     ::-comp/config-spec ::health-endpoint-cfg}
+  [cfg] (create-http-health-endpoint cfg))
 
-  )
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 
 
