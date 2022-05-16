f6bd5807a14c2caf54b0d77d2b3a2a8827cc7e18
Pieter Breed <pieter.breed@gmail.com>
2022-05-16 15:50:40 +0200

    fix bugs in new time component

--- src/afrolabs/components/time.clj
+++ src/afrolabs/components/time.clj
@@ -1,12 +1,14 @@
 (ns afrolabs.components.time
   (:require [java-time :as t]
+            [integrant.core :is ig]
             [afrolabs.components :as -comp]
             [clojure.spec.alpha :as s]))
 
 (defprotocol IClock
   (get-current-time [this] "Retuns the current instant"))
 
-(s/def ::system-time-cfg #{{}})
+(s/def ::system-time-cfg (s/keys :req-un []
+                                 :opt-un []))
 (s/def ::clock #(satisfies? IClock %))
 
 (def system-time-instance
@@ -14,6 +16,6 @@
     IClock
     (get-current-time [_] (t/instant))))
 
-(-comp/defcomponent {::-comp/ig-kw ::system-time
-                     ::-comp/spec  ::system-time-cfg}
+(-comp/defcomponent {::-comp/ig-kw       ::system-time
+                     ::-comp/config-spec ::system-time-cfg}
   [_] system-time-instance)
