52a3a038326ac48db8ae03821bfa91afeac74111
Pieter Breed <pieter@pb.co.za>
2024-04-16 10:46:13 +0200

    add println appender option in logging that can print logging context

--- src/afrolabs/logging.clj
+++ src/afrolabs/logging.clj
@@ -9,12 +9,35 @@
 (def default-min-log-level-maps [[#{"afrolabs.*"} :debug]
                                  ["*" :info]])
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; appender that prints context values to output stream
+
+(let [system-newline (System/getProperty "line.separator")]
+  (defn- atomic-println [x]
+    (print (str x system-newline))
+    (flush)))
+
+(defn context-println-appender
+  "timbre println appender that additionally prints out the logging context as part of the log output.
+
+  based on example at https://github.com/ptaoussanis/timbre/blob/master/src/taoensso/timbre/appenders/example.clj"
+  [_appender-opts_]
+  {:enabled? true
+   :fn       (fn [data]
+               (let [{:keys [output_ context]} data
+                     output                    (force output_)]
+                 (atomic-println (str output
+                                      (when (seq context)
+                                        (str " :: [" context "]"))))))})
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defn configure-logging!
   [& {:keys [disable-stacktrace-colors
              min-level-maps
              min-level
              gck-logging?
              logz-io-logging?
+             println-context?
              ]
       :or {disable-stacktrace-colors false
            min-level-maps            default-min-log-level-maps
@@ -27,10 +50,14 @@
       logz-io-logging? (tas/install (assoc default-tas-config
                                            :msg-key :message))
       :else            (timbre/merge-config!
-                        (when disable-stacktrace-colors
-                          {:output-fn (partial timbre/default-output-fn {:stacktrace-fonts {}})}))))
+                        (merge
+                         (when disable-stacktrace-colors
+                           {:output-fn (partial timbre/default-output-fn {:stacktrace-fonts {}})})
+                         (when println-context?
+                           {:appenders {println (context-println-appender {})}})))))
 
   (timbre/merge-config! {:min-level min-level-maps}))
+
 
 (comment
 
@@ -46,5 +73,4 @@
   (info (Exception. "oeuoeuoeu"))
 
   )
-
 
