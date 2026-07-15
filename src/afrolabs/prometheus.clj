bbacd96f1f1e50b3dba68589b8b302670b54bc94
Pieter Breed <pieter@pb.co.za>
2026-07-15 20:48:58 +0200

    add content-type to prometheus response

--- src/afrolabs/prometheus.clj
+++ src/afrolabs/prometheus.clj
@@ -86,12 +86,13 @@
         ;; give the prometheus write-text-format a writer that is based on the stream that will become the http response body
         ;;
         ;; lifted from: https://nelsonmorris.net/2015/04/22/streaming-responses-using-ring.html
-        (ring-response/response
-         (ring-io/piped-input-stream
-          (fn [output-stream]
-            (with-open [^java.io.BufferedWriter w (io/make-writer output-stream {})]
-              (iapetos.export/write-text-format! w @registry)
-              (.flush w)))))))))
+        (-> (ring-response/response
+             (ring-io/piped-input-stream
+              (fn [output-stream]
+                (with-open [^java.io.BufferedWriter w (io/make-writer output-stream {})]
+                  (iapetos.export/write-text-format! w @registry)
+                  (.flush w)))))
+            (ring-response/content-type "text/plain; version=0.0.4; charset=utf8"))))))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
